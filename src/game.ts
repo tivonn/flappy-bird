a3a56a8a1b4db1349d33d55431ce709bd762de1e
tivonn <tivon.x@qq.com>
2020-02-29 00:22:56 +0800

    feat: add score

--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,13 @@
 import 'phaser'
 import {Tweens, Structs, Physics} from 'phaser'
 
-const HEIGHT = 896
-const gap = 250
-const SPEED = 200
+const HEIGHT: number = 896
+const GAP: number = 250
+const SPEED: number = 200
+const GRAVITY: number = 2700
+const FLY_HEIGHT: number = 700
+const ALIVE_SCORE: number = 1
+const PASS_SCORE: number = 10
 
 enum Status {
   ready,
@@ -15,13 +19,19 @@ export default class FlappyBird extends Phaser.Scene {
   status: Status
   size: Structs.Size
   bird: Phaser.Physics.Arcade.Sprite
-  birdTween: Tweens.Tween
+  birdFly: Tweens.Tween
+  birdFloat: Tweens.Tween
   pipes: Phaser.Physics.Arcade.Group
   makePipeTimer: Phaser.Time.TimerEvent
+  passTimer: Phaser.Time.TimerEvent
+  firstAlivePipe: Physics.Arcade.Image
+  birdAliveTimer: Phaser.Time.TimerEvent
+  score: number
+  scoreText: Phaser.GameObjects.Text
+  changeScoreText: Phaser.GameObjects.Text
 
   constructor () {
     super('Bird')
-    this.status = Status.ready
   }
 
   preload () {
@@ -48,9 +58,11 @@ export default class FlappyBird extends Phaser.Scene {
     }
     platforms.setDepth(9)
     // set bird
-    this.bird = this.physics.add.sprite(384, 448, 'bird')
+    this.bird = this.physics.add.sprite(0, 0, 'bird')
     this.bird.setCollideWorldBounds(true)
     this.physics.add.collider(this.bird, platforms, () => {
+      if (this.status === Status.end) return
+      this.die()
     })
     // bird waves wings
     this.anims.create({
@@ -62,8 +74,8 @@ export default class FlappyBird extends Phaser.Scene {
       frameRate: 10,
       repeat: -1
     })
-    // fly upward
-    this.birdTween = this.tweens.add({
+    // bird fly upward
+    this.birdFly = this.tweens.add({
       targets: this.bird,
       delay: 300,
       duration: 500,
@@ -82,8 +94,35 @@ export default class FlappyBird extends Phaser.Scene {
         }
       }
     })
+    // bird float
+    this.birdFloat = this.tweens.add({
+      targets: this.bird,
+      delay: 0,
+      duration: 800,
+      ease: 'ease',
+      y: {
+        value: '-=100'
+      },
+      yoyo: true,
+      repeat: -1
+    })
     // set pipes
     this.pipes = this.physics.add.group()
+    // set score
+    this.scoreText = this.add.text(this.size.width / 2, 100, '0', {
+      fontSize: '70px',
+      fontFamily: 'fb',
+      align: 'center'
+    })
+    this.scoreText.setOrigin(.5, .5)
+    this.scoreText.setDepth(9)
+    this.changeScoreText = this.add.text(this.size.width / 2, 180, '', {
+      fontSize: '70px',
+      fontFamily: 'fb',
+      align: 'center'
+    })
+    this.changeScoreText.setOrigin(.5, .5)
+    this.changeScoreText.setDepth(9)
     // set event
     this.input.on('pointerdown', function () {
       switch (this.status) {
@@ -99,19 +138,46 @@ export default class FlappyBird extends Phaser.Scene {
   }
 
   ready () {
-    this.pipes.clear(true, true)  // init pipes
-    this.start()
+    // init bird
+    ;(this.bird.body as Physics.Arcade.Body).setAllowGravity(false)
+    this.bird.setAngle(0)
+    this.bird.setVelocityX(0)
+    this.bird.setPosition(this.size.width / 3, 300)
+    this.birdFloat.restart()
+    // init pipes
+    this.pipes.clear(true, true)
+    this.firstAlivePipe = null
+    // init score
+    this.setScore(0)
+    this.status = Status.ready
   }
 
   start () {
+    this.status = Status.playing
+    this.birdFloat.stop()
+    ;(this.bird.body as Physics.Arcade.Body).setAllowGravity()
+    this.fly()
     this.makePipes()
+    this.birdAliveTimer = this.time.addEvent({
+      delay: 1000,
+      callback: this.aliveScore,
+      loop: true,
+      callbackScope: this
+    })
+    // use update lifecycle is too often
+    this.passTimer = this.time.addEvent({
+      delay: 200,
+      callback: this.checkPass,
+      loop: true,
+      callbackScope: this
+    })
   }
 
   fly () {
     this.bird.setAngle(-25)
-    this.birdTween.resume()
-    this.birdTween.restart()
-    this.bird.setVelocityY(-700)
+    this.birdFly.resume()
+    this.birdFly.restart()
+    this.bird.setVelocityY(-FLY_HEIGHT)
   }
 
   makePipes () {
@@ -129,13 +195,13 @@ export default class FlappyBird extends Phaser.Scene {
     up.setName('up')
     up.setFlipY(true)
     let height = up.height
-    let randomHeight = Math.ceil(Math.random() * (this.size.height - 300 - gap)) - 700 + height / 2
+    let randomHeight = Math.ceil(Math.random() * (this.size.height - 300 - GAP)) - 700 + height / 2
     up.y = randomHeight
     let down = this.physics.add.image(this.size.width + 100, 0, 'pipe')
     down.setName('down')
-    down.y = up.y + gap + height
+    down.y = up.y + GAP + height
     this.pipes.addMultiple([up, down])
-    // 目前Phaser有bug，physics.body的类型不正确
+    // the type of physics.body has bug
     ;(up.body as Physics.Arcade.Body).setAllowGravity(false)
     ;(down.body as Physics.Arcade.Body).setAllowGravity(false)
     up.setImmovable()
@@ -164,8 +230,60 @@ export default class FlappyBird extends Phaser.Scene {
     this.makePipeTimer.destroy()
   }
 
+  aliveScore() {
+    this.addScore(ALIVE_SCORE, false)
+  }
+
+  checkPass() {
+    if (this.pipes.getLength() <= 0) return
+    if (!this.firstAlivePipe) {
+      this.firstAlivePipe = this.pipes.getFirstAlive()
+      // only judge one pipe in vertical direction
+      if (this.firstAlivePipe.name == 'down') {
+        this.firstAlivePipe.setActive(false)
+        this.firstAlivePipe = null
+        return
+      }
+    }
+    let x = this.firstAlivePipe.x
+    if (x > this.size.width / 3) return
+    this.addScore(PASS_SCORE, true)
+    this.firstAlivePipe.setActive(false)
+    this.firstAlivePipe = null
+  }
+
+  setScore (score: number) {
+    this.score = score
+    this.scoreText.setText(String(this.score))
+  }
+
+  addScore (changeScore: number, showChange?: boolean) {
+    this.setScore(this.score + changeScore)
+    if (showChange) {
+      this.changeScoreText.setText(`+${changeScore}`)
+      this.time.addEvent({
+        delay: 1000,
+        callback: () => {
+          this.changeScoreText.setText('')
+        },
+        loop: false,
+        callbackScope: this
+      })
+    }
+  }
+
   die () {
+    this.status = Status.end
+    // stop bird
+    this.birdFly.stop(1)
+    this.bird.setAngle(90)
+    this.bird.anims.stop()
+    // stop pipes
     this.stopPipes()
+    this.pipes.setVelocityX(0)
+    // stop timer
+    this.passTimer.destroy()
+    this.birdAliveTimer.destroy()
   }
 }
 
@@ -182,7 +300,7 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 2700 }
+      gravity: { y: GRAVITY }
     }
   }
 }
